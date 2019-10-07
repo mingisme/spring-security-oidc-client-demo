64307e7d2487b93619ecb8ca0e903673263b9c82
swang <mingisme@126.com>
2019-10-07 21:24:27 +0800

    init

---
+++ src/main/java/com/swang/sso/oidcdemo/MyOpenIdConnectConfig.java
@@ -0,0 +1,50 @@
+package com.swang.sso.oidcdemo;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableOAuth2Client
+public class MyOpenIdConnectConfig {
+    @Value("${google.clientId}")
+    private String clientId;
+
+    @Value("${google.clientSecret}")
+    private String clientSecret;
+
+    @Value("${google.accessTokenUri}")
+    private String accessTokenUri;
+
+    @Value("${google.userAuthorizationUri}")
+    private String userAuthorizationUri;
+
+    @Value("${google.redirectUri}")
+    private String redirectUri;
+
+    @Bean
+    public OAuth2ProtectedResourceDetails googleOpenId() {
+        AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
+        details.setClientId(clientId);
+        details.setClientSecret(clientSecret);
+        details.setAccessTokenUri(accessTokenUri);
+        details.setUserAuthorizationUri(userAuthorizationUri);
+        details.setScope(Arrays.asList("openid", "email", "profile"));
+        details.setPreEstablishedRedirectUri(redirectUri);
+        details.setUseCurrentUri(false);
+        return details;
+    }
+
+    @Bean
+    public OAuth2RestTemplate googleOpenIdTemplate(OAuth2ClientContext clientContext) {
+        return new OAuth2RestTemplate(googleOpenId(), clientContext);
+    }
+}
